361b0169b6dc343b337433d0fe403e86fa4c7caf
Sonny Scroggin <sonny@scrogg.in>
2019-12-18 22:49:39 -0600

    swap lazy_static for once_cell

--- native/franz/src/task.rs
+++ native/franz/src/task.rs
@@ -1,13 +1,14 @@
+use once_cell::sync::Lazy;
 use std::future::Future;
 use tokio::runtime::{Builder, Runtime};
 use tokio::task::JoinHandle;
 
-lazy_static::lazy_static! {
-    static ref TOKIO: Runtime = Builder::new()
+static TOKIO: Lazy<Runtime> = Lazy::new(|| {
+    Builder::new()
         .threaded_scheduler()
         .build()
-        .expect("Franz.Native: Failed to start tokio runtime");
-}
+        .expect("Franz.Native: Failed to start tokio runtime")
+});
 
 pub fn spawn<T>(task: T) -> JoinHandle<T::Output>
 where
